89128723ce3699dbbc9f2886c53b9e463c17c4be
ktlearningtocode <74963136+ktlearningtocode@users.noreply.github.com>
2025-12-22 00:57:54 +0000

    Implement scroll to top button functionality

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
+const scrollToTopBtn =document.getElementById("scrollToTopBtn");
 
-function scrollToTop() { 
-    window.scrollTo({ 
-top:0, 
-behavior:'smooth'
-}); 
+window.onscroll= function() {
+     
+if (document.body.scrollTop > 100  ||
+document.documentElement.myBtn.classList.add('show')) { 
+    mybutton.style.display= "block";
+} else { 
+scrollToTopBtn.classList.remove('show'); 
 }
+};
+scrollToTopBtn.addEventListener("click",function() {
+    window.scrollTo({
+        top:0,
+        behaviour:"smooth"
+    });
+
+    });
+    
+
+function topFunction() {
+document.body.scrollTop=0;
+document.documentElement.scrollTop=0; }
 
 
+ function openNav() { 
+document.getElementById("mySidenav").style.width="100%"; 
 
+} 
+function closeNav() {
+    document.getElementById("mySidenav").style.width="0";
+
+}

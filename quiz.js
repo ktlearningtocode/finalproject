0a6a30fe6df13abfb7a8ca34d4e20aa6bad9fda8
ktlearningtocode <74963136+ktlearningtocode@users.noreply.github.com>
2026-04-11 01:15:14 +0100

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -37,8 +37,74 @@ answers:[
 {text:"frequent updates" , correct:false},
 {text:"data relevance", correct:false},
 ] 
+},
+
+{
+  question:"what is the ability to ensure that assets are viewable only by authorised people known as?",
+answers:[
+{text:"Planning and direction", correct:false}, 
+{text:"confidentiality", correct:true},
+{text:"Updates", correct:false},
+{text:"passwords", correct:false},
+] 
+},
+ {
+  question:"In a DDOS attack what is compromised?",
+answers:[
+{text:"Planning", correct:false}, 
+{text:"Directions", correct:false},
+{text:"Updates", correct:false},
+{text:"Availabilty", correct:true},
+] 
+},
+ {
+  question:"What matches the technology that makes sure the correct individuals access the correct resources at the right times for the correct reasons?",
+answers:[
+{text:"Planning and direction", correct:false}, 
+{text:"Updates and availability", correct:false},
+{text:"Identity and Access management", correct:true},
+{text:"Information overload", correct:false},
+] 
+},
+
+{
+  question:"What is the weakness in a system known as?",
+answers:[
+{text:"Vulnerability", correct:true}, 
+{text:"Updates and availability", correct:false},
+{text:"Weakness", correct:false},
+{text:"Authorisation", correct:false},
+] 
+},
+{
+  question:"What is the course of action that identifies particular measures that can be used for particular stages of attack?",
+answers:[
+{text:"COA(cause of action)", correct:false}, 
+{text:"Information Gathering", correct:false},
+{text:"Attack Cycle", correct:false},
+{text:"Cyber kill chain", correct:true},
+] 
+},
+{
+  question:"What does secuirty threat entail?",
+answers:[
+{text:"Entering a wrong password", correct:false}, 
+{text:"forgetting an email address", correct:false},
+{text:"Someone or something that makes a situation unsafe", correct:true},
+{text:"indication of trouble approaching", correct:false},
+] 
+},
+{
+  question:"What are three processes buisnesses include with cyber threat intellignce",
+answers:[
+{text:"Entering a wrong password, forgetting an email address and making a situation unsafe ", correct:false}, 
+{text:"Cyber kill chain, Attack Cycle and COA(cause of action)", correct:false},
+{text:"confidentiality, Updates and availability and Vulnerability", correct:false},
+{text:"Strategic level, operational level and tactical level", correct:true},
+] 
 }
 ]; 
+
 const questionElement = document.getElementById('question'); 
 const answerButtons = document.getElementById('answer-buttons'); 
 const nextButton = document.getElementById('next-btn');
